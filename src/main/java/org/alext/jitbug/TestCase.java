e0b3b535fcb2064f95720fec164d2c4f10d28579
Lukas Rytz <lukas.rytz@gmail.com>
2016-06-27 10:37:10 +0200

    simplifications

--- src/main/java/org/alext/jitbug/TestCase.java
+++ src/main/java/org/alext/jitbug/TestCase.java
@@ -1,22 +1,17 @@
 package org.alext.jitbug;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 
 import scala.Tuple6;
 
 public class TestCase {
-
     public static void main(String[] args) throws InterruptedException {
         int count = 10;
-        for (int i = 0; i< 3; i++) {
+        for (int i = 0; i < 3; i++) {
             CountDownLatch l = new CountDownLatch(count);
-            for (int j = 0; j<count; j++) {
+            for (int j = 0; j < count; j++) {
                 Thread t = new Thread(new Worker(l));
                 t.start();
                 System.out.println(" Started thread: " + t.getName());
@@ -25,7 +20,7 @@ public class TestCase {
         }
     }
 
-    public static class Worker implements Runnable  {
+    public static class Worker implements Runnable {
 
         private CountDownLatch latch;
 
@@ -37,18 +32,14 @@ public class TestCase {
         public void run() {
             try {
                 List<String> demoIds = new ArrayList<>();
-                for (int i = 1; i<= 20; i++) {
-                    demoIds.add("demoID="+i);
-                }
-                TimeShiftingViewingPeriod[] periods = TimeShiftingViewingPeriod.values();
-                Contribution[] contributions = Contribution.values();
-                Random r = new Random(System.nanoTime());
-                for (long i =0; i<40_000l; i++) {
-                    TimeShiftingViewingPeriod viewingPeriod = periods[r.nextInt(periods.length)];
-                    Contribution contribution = contributions[r.nextInt(contributions.length)];
-                    Set<Tuple6<Long, Long, Byte, String, Byte, String>> res = doStuff(demoIds, 192837934738l, 17210l, viewingPeriod, contribution, "0");
-                    if (res.iterator().next()._3() == null ) {
-                        System.out.println(" Start reproducing: Thread"+ Thread.currentThread().getName() +" at step: " + i + " res:  " + res);
+                for (int i = 1; i <= 20; i++)
+                    demoIds.add("demoID=" + i);
+
+                for (long i = 0; i < 40_000l; i++) {
+                    TimeShiftingViewingPeriod viewingPeriod = TimeShiftingViewingPeriod.LIVE_7;
+                    Tuple6<Long, Long, Byte, String, Byte, String> res = doStuff(demoIds, 192837934738l, viewingPeriod, "0");
+                    if (res._3() == null) {
+                        System.out.println(" Start reproducing: Thread" + Thread.currentThread().getName() + " at step: " + i + " res:  " + res);
                         break;
                     }
                 }
@@ -57,15 +48,38 @@ public class TestCase {
             }
         }
 
-        private Set<Tuple6<Long, Long, Byte, String, Byte, String>> doStuff(List<String> demoIds,
-                long programId, long date, TimeShiftingViewingPeriod viewingPeriod, Contribution contribution, String marketBreak) {
-            Set<Tuple6<Long, Long, Byte, String, Byte, String>> res = new HashSet<>();
-            demoIds.forEach(demoId -> res.add(new Tuple6<>(date, programId, viewingPeriod.getId(), demoId, contribution.getId(), marketBreak)));
-
-            return res;
-
+        private Tuple6<Long, Long, Byte, String, Byte, String> doStuff(
+                List<String> demoIds,
+                long programId,
+                TimeShiftingViewingPeriod viewingPeriod,
+                String marketBreak) {
+            C<Tuple6<Long, Long, Byte, String, Byte, String>> r = new C<>(null);
+            demoIds.forEach(demoId -> r.t = new Tuple6<>(null, programId, viewingPeriod.getId(), null, null, marketBreak));
+            return r.t;
         }
 
     }
 
+    static class C<T> {
+        T t;
+
+        public C(T t) {
+            this.t = t;
+        }
+    }
+
+    static enum TimeShiftingViewingPeriod {
+        LIVE_7((byte) 9);
+
+        private Byte id;
+
+        TimeShiftingViewingPeriod(byte id) {
+            this.id = id;
+        }
+
+        public Byte getId() {
+            return id;
+        }
+
+    }
 }
